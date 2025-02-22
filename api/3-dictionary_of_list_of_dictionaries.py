5a8d3a5b74921e2e37e20b129033c6929560ff2d
h-mutumwinka <h.mutumwink@alustudent.com>
2025-02-22 18:07:52 +0200

    3-dictionary_of_list_of_dictionaries.py

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -1,27 +1,35 @@
 #!/usr/bin/python3
-"""Python script that exports data in the JSON format"""
+"""
+Python script that exports data in the JSON format
+"""
 import json
 import requests
 
 
-
 def get_all_users():
-    """Get the list of all users"""
+    """
+    Get the list of all users
+    """
     url = "https://jsonplaceholder.typicode.com/users"
     response = requests.get(url)
     return response.json()
 
 
 def get_user_todos(user_id):
-    """Get the TODO list for a given user ID"""
+    """
+    Get the TODO list for a given user ID
+    """
     url = "https://jsonplaceholder.typicode.com/todos"
     response = requests.get(url, params={"userId": user_id})
     return response.json()
 
 
 def export_all_todos_to_json(users):
-    """Export all users' TODO lists to a JSON file"""
-    data = {user["id"]: [
+    """
+    Export all users' TODO lists to a JSON file
+    """
+    data = {
+        user["id"]: [
             {
                 "task": todo["title"],
                 "completed": todo["completed"],
@@ -34,7 +42,9 @@ def export_all_todos_to_json(users):
 
 
 def main():
-    """Main function to fetch users and their TODO lists, then export to JSON"""
+    """
+    Main function to fetch users and their TODO lists, then export to JSON
+    """
     users = get_all_users()
     export_all_todos_to_json(users)
 
